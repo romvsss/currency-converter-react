4842f63ca4672d766c4c218e717791f5ec8de338
Oskar Romanowicz <oskar.romanowicz@onet.pl>
2025-10-21 12:14:36 +0200

    abc

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -2,13 +2,27 @@ import { StyledClock } from './styled'
 import { useCurrentDate } from '../useCurrentDate';
 
 const Clock = () => {
-    const formattedDate = useCurrentDate();
+    const currentDate = useCurrentDate();
+    
+    const formatDateTime = (date) => {
+        const days = ['niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota'];
+        const dayName = days[date.getDay()];
+        
+        return `Dziś jest ${dayName}, ${date.toLocaleString('pl-PL', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        })}`;
+    };
 
     return (
         <StyledClock>
-            {formattedDate}
+            {formatDateTime(currentDate)}
         </StyledClock>
     );
-}
+};
 
 export default Clock;
--- src/useCurrentDate.js
+++ src/useCurrentDate.js
@@ -13,19 +13,5 @@ export const useCurrentDate = () => {
         };
     }, []);
 
-    const formatDateTimeWithDay = (date) => {
-        const days = ['niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota'];
-        const dayName = days[date.getDay()];
-        
-        return `Dziś jest ${dayName}, ${date.toLocaleString('pl-PL', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit'
-        })}`;
-    };
-
-    return formatDateTimeWithDay(currentDate);
+    return currentDate;
 };
