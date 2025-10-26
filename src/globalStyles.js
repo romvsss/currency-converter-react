79f693d0daca89b7d04a40a613aa992a26bb8fa2
Oskar Romanowicz <oskar.romanowicz@onet.pl>
2025-10-26 19:32:26 +0100

    css edited

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -20,6 +20,11 @@ body {
 export const Footer = styled.footer`
   text-align: center;
   color: white;
+  font-size: 1.2rem;
+  font-weight: bold;
+  padding: 20px;
+  background: rgba(0, 0, 0, 0.7);
+  margin-top: 2rem;
   text-shadow: 
     -1px -1px 0 #000000ff,
      1px -1px 0 #000000ff,
